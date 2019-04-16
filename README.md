f9ea349f68683111fa8df35378246531276162f4
John Doe <michael.list@dataslot.de>
2019-04-16 21:15:15 +0200

    Add Seedstudio sunlight sensor

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Arduino Weatherstation with Lora
 
 # Used libraries
 * https://github.com/Seeed-Studio/Seeed_BME680
-* 
+* https://github.com/SeeedDocument/Grove-Sunlight_Sensor/
--- arduino_lora_weatherstation.ino
+++ arduino_lora_weatherstation.ino
@@ -1,9 +1,12 @@
 #include <SPI.h> // For lora
 #include <LoRa.h> // For lora
 #include "seeed_bme680.h" // Humidity, temperature and pressure sensor
+#include <Wire.h> // For sunlight sensor
+#include "SI114X.h" // For sunlight sensor
 
 #define IIC_ADDR uint8_t(0x76) // Address of BME680 sensor
 Seeed_BME680 bme680(IIC_ADDR); // IIC PROTOCOL
+SI114X SI1145 = SI114X(); // Sunlight sensor
 
 void setup() {
 // For debugging
@@ -16,6 +19,11 @@ void setup() {
   }
 
   while (!bme680.init()) {
+//  If init failed, wait and try again
+    delay(10000);
+  }
+
+  while (!SI1145.Begin()) {
 //  If init failed, wait and try again
     delay(10000);
   }
@@ -35,6 +43,12 @@ void loop() {
   LoRa.print(bme680.sensor_result_value.pressure / 100); // In hpa
   LoRa.print(";");
   LoRa.print(bme680.sensor_result_value.humidity); // In %
+  LoRa.print(";");
+  LoRa.print(SI1145.ReadVisible()); // Visible light in lm (lumen)
+  LoRa.print(";");
+  LoRa.print(SI1145.ReadIR()); // Infrared light in lm
+  LoRa.print(";");
+  LoRa.print((float)SI1145.ReadUV()/100); // Ultraviolet light in lm
   LoRa.print(";");
   LoRa.endPacket();
 
